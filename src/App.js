c2548f65f78faf62985b3550090fa03a2542867b
taylorpapke <taylorpapke@gmail.com>
2025-11-18 16:55:44 -0800

    set up new form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css';
 import Main from './pages/Main'
 import Contact from './pages/Contact'
+import ContactThankYou from './pages/ContactThankYou';
 import About from './pages/About'
 import PortfolioPage from './pages/PortfolioPage'
 import DevelopersPage from './pages/DevelopersPage'
@@ -15,6 +16,7 @@ function App() {
             <Route exact path="/" element={<Navigate to="/Home" />} />
             <Route path='/Home' element={<Main />} />
             <Route path='/Contact' element={<Contact />} />
+            <Route path='/Contact-Thank-You' element={<ContactThankYou />} />
             <Route path='/About' element={<About />} />
             <Route path='/Portfolio' element={<PortfolioPage />} />
             <Route path='/Developers' element={<DevelopersPage />} />
---
+++ src/pages/ContactThankYou.js
@@ -0,0 +1,84 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Helmet } from "react-helmet";
+
+function ContactThankYou() {
+  return (
+    <>
+      <Helmet>
+        <title> Next Wave Dev - Thank You </title>
+      </Helmet>
+      <Navbar />
+      <div style={{ minHeight: "calc(100vh - 95px)", height: "1px" }}>
+        <div
+          style={{
+            marginTop: "10vh",
+            marginBottom: "30vh",
+            marginLeft: "2rem",
+            marginRight: "2rem",
+            height: "100%",
+          }}
+        >
+          <div
+            style={{
+              ...flexParent,
+              height: "100%",
+            }}
+          >
+            <div
+              style={{
+                ...flexChild,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                textAlign: "center",
+                fontSize: "1.5rem",
+                fontWeight: "500",
+                color: "#333",
+              }}
+              className={"col-sm-6"}
+            >
+              <p>Thank you! Your response has been submitted.</p>
+            </div>
+            <div
+              style={{
+                ...flexChild,
+                display: "flex",
+                justifyContent: "center",
+              }}
+              className={"col-sm-6"}
+            >
+              <iframe
+                title="contact-page-map"
+                src="https://maps.google.com/maps?q=12356+NORTHUP+WAY+STE+E,+BELLEVUE,+WA,+98005-5176,+UNITED+STATES&t=&z=13&ie=UTF8&iwloc=&output=embed"
+                width="80%"
+                height="400"
+                allowFullScreen=""
+                loading="lazy"
+                style={{ border: "none" }}
+                referrerPolicy="no-referrer-when-downgrade"
+                id={"contact-page-map"}
+              ></iframe>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export const flexChild = {
+  position: "relative",
+  width: "100%",
+  minHeight: "1px",
+  paddingRight: "15px",
+  paddingLeft: "15px",
+};
+
+export const flexParent = {
+  display: "flex",
+  flexWrap: "wrap",
+};
+
+export default ContactThankYou;
